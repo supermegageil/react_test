5224ea022ee51a5d3ceb1a7f4a29758cef16419c
tubi55 <tubi55@nate.com>
2021-12-21 12:43:35 +0900

    컴포넌트에 이벤트 연결하기

--- src/components/Panel.js
+++ src/components/Panel.js
@@ -2,16 +2,26 @@ function Panel() {
 	const arr = ['Blizzards', 'Calm', 'Dusty_Road', 'Escape', 'Payday', 'Retreat', 'Seasonal', 'Vespers'];
 	const path = process.env.PUBLIC_URL;
 	const deg = 360/arr.length;	
+	const btnStyle = {position: 'fixed', top:0, left:0}
+	
+	function changeArr(txt){
+		arr[0] = txt;
+		console.log(arr);
+	}
 
 	return (
 		<>
+		<button style={btnStyle} onClick={()=>changeArr('Escape')}>button</button>
 		{
-			arr.map((data,index)=>{				
+			arr.map((data,index)=>{	
+				let style = {transform: `rotate(${deg*index}deg) translateY(-100vh)`}	
+				let imgSrc = `${path}/img/${data}.jpg`
+
 				return (
-					<article key={index} style={{transform: `rotate(${deg*index}deg) translateY(-100vh)`}}>
+					<article key={index} style={style}>
 						<div className="inner">
 							<div className="pic">
-								<img src={`${path}/img/${data}.jpg`} />
+								<img src={imgSrc} />
 							</div>
 
 							<h2>{data}</h2>				
@@ -20,8 +30,7 @@ function Panel() {
 				)
 			})
 		}
-		</>
-		
+		</>		
 	)
 }
 
