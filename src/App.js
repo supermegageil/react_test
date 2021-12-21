398ec25d106d7008806ffb77daef06dcbd93dba5
tubi55 <tubi55@nate.com>
2021-12-21 10:49:37 +0900

    step3 반복 컨텐츠 UI 스타일링

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './css/style.css';
 import {Logo, Menu} from './components/Header.js';
 import Footer from './components/Footer.js';
 
+const name1 = "DAVID2";
+
 function App() {
 	return (
 		<div className="App">
@@ -9,6 +11,49 @@ function App() {
 			<figure>
 				<Logo />
 				<Menu />
+
+				<section>
+					<article>
+						<div className="inner">
+							{name1}
+						</div>
+					</article>
+					<article>
+						<div className="inner">
+							ANDY
+						</div>
+					</article>
+					<article>
+						<div className="inner">
+							MICHAEL
+						</div>
+					</article>
+					<article>
+						<div className="inner">
+							JULIA
+						</div>
+					</article>
+					<article>
+						<div className="inner">
+							CHRISTOPER
+						</div>
+					</article>
+					<article>
+						<div className="inner">
+							EMMA
+						</div>
+					</article>
+					<article>
+						<div className="inner">
+							PAUL
+						</div>
+					</article>
+					<article>
+						<div className="inner">
+							CATHERINE
+						</div>
+					</article>
+				</section>
 
 				<Footer />				
 			</figure>
