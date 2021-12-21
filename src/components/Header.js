d43f789088591e394743764b2cc91dc352e62fae
tubi55 <tubi55@nate.com>
2021-12-21 19:14:02 +0900

    logo change office

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 function Logo(){
     return (        
         <h1>
-            <strong>DECODELAB2</strong><br />
+            <strong>DECODELAB3</strong><br />
             <span>UI/UX DESIGN&DEVELOPMENT</span>
         </h1> 
     )
